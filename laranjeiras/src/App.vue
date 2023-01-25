3f0fddbd430ec158bc52670e4c7593107f440bb4
lyoto07 <guilherme-lira@outlook.com.br>
2023-01-25 19:27:54 -0300

    Criação do menu e adiçãode imagens

--- laranjeiras/src/App.vue
+++ laranjeiras/src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
-
 export default defineComponent({
   name: 'App',
   components: {
@@ -16,12 +15,5 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
