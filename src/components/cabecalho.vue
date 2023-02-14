01ef5cb9461d2b3763a408bff3e91452a2792c6c
lyoto07 <guilherme-lira@outlook.com.br>
2023-02-13 21:05:00 -0300

    build project

---
+++ src/components/cabecalho.vue
@@ -0,0 +1,18 @@
+<template>  
+    <div class="d-flex justify-content-end">
+        <img src="../assets/img/fundo_header.svg" alt="">
+    </div>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'cabecalhoPrincipal',
+    })
+</script>
+
+<style>
+
+</style>
